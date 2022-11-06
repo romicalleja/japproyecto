5c573afbb8ad03a96f59926d864c3d69346a816a
Romina Calleja <romycalleja.12.08@gmail.com>
2022-11-06 19:06:09 -0300

    ,,

---
+++ js/my-profile.js
@@ -0,0 +1,19 @@
+let nombre = document.getElementById("nombre")
+let apellido = document.getElementById("apellido")
+let nombre2 = document.getElementById("nombre2")
+let apellido2 = document.getElementById("apellido2")
+let email = document.getElementById("email")
+let contacto = document.getElementById("contacto")
+
+
+function guardar(){
+    console.log(nombre.value)
+    console.log(apellido.value)
+    console.log(email.value)
+
+}
+
+// document.addEventListener("DOMContentLoaded", function(e){
+//      email.value.innerHTML=localStorage.getItem("mail")
+//     console.log(email)
+// } )
