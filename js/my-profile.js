aa70ed911399fe56f3e5eabf8c4da46c4f8c234b
Romina Calleja <romycalleja.12.08@gmail.com>
2022-11-08 22:18:59 -0300

    ghjg

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,19 +1,25 @@
-let nombre = document.getElementById("nombre")
-let apellido = document.getElementById("apellido")
-let nombre2 = document.getElementById("nombre2")
-let apellido2 = document.getElementById("apellido2")
-let email = document.getElementById("email")
-let contacto = document.getElementById("contacto")
+let nombre = document.getElementById("nombre");
+let apellido = document.getElementById("apellido");
+let nombre2 = document.getElementById("nombre2");
+let apellido2 = document.getElementById("apellido2");
+let email = document.getElementById("email");
+let contacto = document.getElementById("contacto");
 
-
-function guardar(){
-    console.log(nombre.value)
-    console.log(apellido.value)
-    console.log(email.value)
-
+function guardar() {
+  localStorage.setItem("mail", email.value);
+  localStorage.setItem("nombre", nombre.value);
+  localStorage.setItem("nombre2", nombre2.value);
+  localStorage.setItem("apellido", apellido.value);
+  localStorage.setItem("apellido2", apellido2.value);
+  localStorage.setItem("contacto", contacto.value);
+  alert("informacion actualizada con exito");
 }
 
-// document.addEventListener("DOMContentLoaded", function(e){
-//      email.value.innerHTML=localStorage.getItem("mail")
-//     console.log(email)
-// } )
+document.addEventListener("DOMContentLoaded", function (e) {
+  email.value = localStorage.getItem("mail");
+  nombre.value = localStorage.getItem("nombre");
+  nombre2.value = localStorage.getItem("nombre2");
+  apellido.value = localStorage.getItem("apellido");
+  apellido2.value = localStorage.getItem("apellido2");
+  contacto.value = localStorage.getItem("contacto");
+});
